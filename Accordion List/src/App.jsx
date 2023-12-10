ec3981116224593ce92c08f1eaf31022e8cbccd8
kartick17 <karticksadhu0@gmail.com>
2023-12-10 22:21:06 +0530

    Modifi accordion list application

--- Accordion List/src/App.jsx
+++ Accordion List/src/App.jsx
@@ -27,27 +27,32 @@ function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null)
   return (
     <div className="accordion">
       {
-        data.map((el, i) => <AccordionList num={i} text={el.text} title={el.title} key={el.title} />)
+        data.map((el, i) =>
+          <AccordionList num={i} title={el.title} key={el.title} curOpen={curOpen} onOpen={setCurOpen}>
+            {el.text}
+          </AccordionList>
+        )
       }
     </div>
   )
 }
 
-function AccordionList({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false)
+function AccordionList({ num, title, children, curOpen, onOpen }) {
+  const isOpen = curOpen === num
 
   function handleToggle() {
-    setIsOpen(isOpen => !isOpen)
+    onOpen(isOpen ? null : num)
   }
 
   return <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
     <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
     <p className='title'>{title}</p>
     <p className='icon'>{isOpen ? '-' : '+'}</p>
-    {isOpen && <div className='content-box'>{text}</div>}
+    {isOpen && <div className='content-box'>{children}</div>}
   </div>
 }
 
